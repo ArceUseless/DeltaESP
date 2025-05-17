6bdf879cfa281d776aac97555bbfc4a5de3c1189
ArceUseless <36188730+ArceUseless@users.noreply.github.com>
2025-05-18 00:14:03 +0200

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyC6-4IIKruik-qY0t2MMvtTA2Rd8rDG2-M",
+        authDomain: "deltaesp-6e9e7.firebaseapp.com",
+        projectId: "deltaesp-6e9e7",
+        storageBucket: "deltaesp-6e9e7.appspot.com",
+        messagingSenderId: "116947896463",
+        appId: "1:116947896463:web:0c3ca5985a2b88011548fa",
+        measurementId: "G-YNT24PPPVT"
+    }
+  };
